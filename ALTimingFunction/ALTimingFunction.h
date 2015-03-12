aa4ee5f590ad073200d1bd79f7753cab2cc3b91d
0dayZh <0day.zh@gmail.com>
2015-03-12 15:06:19 +0800

    Add project.

---
+++ ALTimingFunction/ALTimingFunction.h
@@ -0,0 +1,9 @@
+//
+//  ALTimingFunction.h
+//  ALTimingFunction
+//
+//  Created by 0dayZh on 15/3/12.
+//  Copyright (c) 2015 0dayZh. All rights reserved.
+//
+
+#import "CAMediaTimingFunction+AdditionalEquations.h"
